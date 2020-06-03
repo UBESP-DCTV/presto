2671cf6462ab74af611d6c2e8a291622a1d04126
Corrado <corrado.lanera@gmail.com>
2020-06-03 17:09:07 +0200

    refactor 01-setup.R

--- dev/01_setup.R
+++ dev/01_setup.R
@@ -1,51 +1,77 @@
+# Wed Jun  3 17:08:37 2020 ------------------------------
+
+# Creation --------------------------------------------------------
+
 devubesp::create_ubesp_analysis("presto")
 use_directory("dev", TRUE)
-# created this file
+
+
+# Documentation ---------------------------------------------------
 
 usethis::use_gpl3_license()
-use_readme_rmd()
-usethis::use_code_of_conduct()
-usethis::use_lifecycle_badge("Maturing")
 usethis::use_roxygen_md()
 usethis::use_package_doc()
 usethis::use_news_md()
+
+use_readme_rmd()
 usethis::use_logo("~/Downloads/prestoinsieme.png")
+usethis::use_lifecycle_badge("Maturing")
+usethis::use_code_of_conduct()
+usethis::use_spell_check()
 
-usethis::use_testthat()
-usethis::use_dev_package("autotestthat", type = "Suggests")
-usethis::use_data()
 
-usethis::use_git()
-usethis::git_vaccinate()
-usethis::use_github("UBESP-DCTV")
-usethis::use_tidy_github()
-usethis::use_github_action("lint")
-usethis::use_github_action("test-coverage")
-usethis::use_coverage()
-usethis::use_tidy_github_actions()
-use_tidy_description()
-use_tidy_support()
-
-usethis::use_pipe()
-usethis::use_test("pipe")
-usethis::use_tibble()
+# Interface -------------------------------------------------------
 
 # remotes::install_github("CorradoLanera/usethis")
 usethis::use_dev_package("usethis")
 usethis::use_ui()
-usethis::use_spell_check()
 
+usethis::use_data()
+usethis::use_pipe()
+usethis::use_tibble()
 usethis::use_r("utils")
-usethis::use_test("utils")
-# added R/utils.R
 
+
+
+
+# git/github ------------------------------------------------------
+
+usethis::use_git()
+usethis::git_vaccinate()
+
+usethis::use_github("UBESP-DCTV")
+usethis::use_tidy_github()
+
+
+# CI --------------------------------------------------------------
+
+usethis::use_testthat()
+usethis::use_dev_package("autotestthat", type = "Suggests")
+
+usethis::use_test("pipe")
+usethis::use_test("utils")
+
+usethis::use_tidy_github_actions()
+usethis::use_github_action("lint")
+usethis::use_github_action("test-coverage")
+usethis::use_coverage()
+
+
+
+# finalization ----------------------------------------------------
 
 usethis::use_tidy_description()
+spelling::spell_check_package()
+lintr::lint_package()
+
 devtools::document(
   roclets = c('rd', 'collate', 'namespace', 'vignette')
 )
 
+
+
+# renv ------------------------------------------------------------
+
 renv::init()
 
-lintr::lint_package()
 
